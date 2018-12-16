6b1d544cc79bf21e20f2ceaf3078068777268186
Skorzec francois <fskorzec@outlook.com>
2018-12-15 23:26:42 -0500

    Refactoring

--- back/src/modules/HookReceiver.ts
+++
@@ -1,9 +0,0 @@
-import * as nc from "shadow-nucleus";
-
-
-export default class implements nc.IModuleEntryPoint {
-  entryPoint(api: nc.IApi): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-
-}
